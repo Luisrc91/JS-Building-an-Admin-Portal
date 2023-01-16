376d2b3bee57e255c272a79cdceea3ca3b5d7d48
Luis <luisrc91@live.com>
2023-01-16 18:05:43 -0500

    first commit

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,52 @@
 
 // Your Code Here
+// book title, input, button
+//event listener to save button book udate
+
+const { json } = require("body-parser")
+
+// fech request 
+async function main(){
+    const response = await fetch('http://localhost:3001/listBooks')
+    const books = await response.json()
+    // console.log(book)
+
+    books.forEach(renderBook)
+    // console.log(book)
+}
+function renderBook(book){
+    console.log(book)
+    const root = document.getElementById('root')
+    const li = document.createElement('li')
+    li.textContent = book.title
+
+    const input = document.createElement('input')
+    input.value = book.quantity
+   
+    const saveButton = document.createElement('button')
+    saveButton.textContent = 'save'
+
+    // const body = {
+    //     id = book.id,
+    //     quantity: input.value
+    // }
+
+    saveButton.addEventListener('click', () => {
+        const body = {
+            id : book.id,
+            quantity: input.value
+        }
+        fetch('http://localhost:3001/updateBook',)
+            method: 'PACTH',
+            headers: {
+            'content-type': 'application/json'
+            },
+            body: JSON.stringify(body)
+    })
+   
+    li.append(input, saveButton)
+
+    root.appendChild(li)
+}
+
+ main()
